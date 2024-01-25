081b3d71d86db47476df58242ee2f382a64dbd90
edurs2602 <edurs.2602@gmail.com>
2024-01-25 11:42:07 -0300

    heroku settings

--- livros/livros/settings.py
+++ livros/livros/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +123,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.setting(locals())
