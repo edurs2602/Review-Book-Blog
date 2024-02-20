5a684730065349f25ebf1b2025959cf928470ecd
edurs2602 <edurs.2602@gmail.com>
2024-02-20 13:51:29 -0300

    fix: static path

--- livros/livros/settings.py
+++ livros/livros/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
